b75868abc8da3fe141c5b501e15c0ac3b5b7e8d2
te9yie <te9yie@users.noreply.github.com>
2022-11-13 14:52:43 +0900

    add main

---
+++ src/pch.h
@@ -0,0 +1,12 @@
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+
+#include <SDL.h>
+
+#include <cstdlib>
+#include <memory>
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdlrenderer.h"
